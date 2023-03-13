59bf3bcf37f7e4346f157e471625898e3da05877
AlexisNoukan <alexisnoukan@gmail.com>
2023-03-13 11:39:59 +0100

    start: app ui/ux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
-import "./App.css";
 
 function App() {
-  return <div className="App">calculatrice</div>;
+  return (
+    <div className=" flex flex-col item-center gap-2 ">
+      <div>
+        <h1>calculatrice</h1>
+      </div>
+
+      <div className="container flex justify-center">
+        <div className="flex">
+          <p>C</p>
+          <p>C</p>
+          <p>C</p>
+          <p>C</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
